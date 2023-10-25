b26ef234be356687edd0f9fc457b5b434edef7fb
Matheus Barboza <matheus.barboza102@gmail.com>
2023-10-25 19:31:38 -0300

    feat: add Cart component

---
+++ src/components/ui/cart.tsx
@@ -0,0 +1,7 @@
+const Cart = () => {
+  return (
+    <span>cart</span>
+  )
+}
+
+export default Cart
--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -9,6 +9,7 @@ import { Avatar } from "./avatar"
 import { AvatarFallback, AvatarImage } from "@radix-ui/react-avatar"
 import { Separator } from "./separator"
 import Link from "next/link"
+import Cart from "./cart"
 
 const Header = () => {
   const { status, data } = useSession()
@@ -102,9 +103,17 @@ const Header = () => {
         </h1>
       </Link>
 
-      <Button size="icon" variant="outline">
-        <ShoppingCartIcon />
-      </Button>
+      <Sheet>
+        <SheetTrigger asChild>
+          <Button size="icon" variant="outline">
+            <ShoppingCartIcon />
+          </Button>
+        </SheetTrigger>
+
+        <SheetContent>
+          <Cart />
+        </SheetContent>
+      </Sheet>
     </Card>
   )
 }
